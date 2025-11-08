461c4b2a80c08cfbe42abae0dd24678562e43b9e
Алексей Пилюгин <aleksinap@yandex.ru>
2025-11-08 21:32:18 +0300

    second commit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
 )
 
 func HandleMain(w http.ResponseWriter, r *http.Request) {
@@ -25,7 +26,20 @@ func HandleUpload(w http.ResponseWriter, r *http.Request) {
 
 	file, handler, err := r.FormFile("myFile")
 	if err != nil {
-		http.Error(w, "File upload failed", http.StatusInternalServerError)
+
+		text := r.FormValue("text")
+		if text == "" {
+			http.Error(w, "no input provided", http.StatusBadRequest)
+			return
+		}
+		os.WriteFile("random.txt", []byte(text), 0755)
+		morse := morse.ToMorse(text)
+		os.WriteFile("morse.txt", []byte(morse), 0755)
+
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(morse))
+		return
 	}
 	defer file.Close()
 
@@ -39,7 +53,7 @@ func HandleUpload(w http.ResponseWriter, r *http.Request) {
 
 	err = os.WriteFile(time.Now().UTC().String(), data, 0755)
 	if err != nil {
-		http.Error(w, "File write failure", http.StatusInternalServerError)
+		http.Error(w, "file write failure", http.StatusInternalServerError)
 		return
 	}
 
