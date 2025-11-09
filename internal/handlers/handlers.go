6fd7ef4054c35bc0d3ca401b3415059aed4bad64
Алексей Пилюгин <aleksinap@yandex.ru>
2025-11-09 11:47:12 +0300

    sixth commit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,8 +1,11 @@
 package handlers
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
@@ -21,30 +24,36 @@ func HandleMain(w http.ResponseWriter, r *http.Request) {
 }
 
 func HandleUpload(w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(10 << 20)
+	if err := r.ParseMultipartForm(10 << 20); err != nil {
+		http.Error(w, "Form parse error", http.StatusBadRequest)
+		return
+	}
 
 	file, handler, err := r.FormFile("myFile")
 	if err != nil {
-		http.Error(w, "File upload failed", http.StatusInternalServerError)
+		http.Error(w, "File upload error", http.StatusInternalServerError)
 		return
 	}
 	defer file.Close()
 
-	data, err := os.ReadFile(handler.Filename)
+	data, err := io.ReadAll(file)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, "File read error", http.StatusInternalServerError)
 		return
 	}
 
-	data = service.Convert(data)
+	converted := service.Convert(data)
 
-	err = os.WriteFile(time.Now().UTC().String(), data, 0755)
-	if err != nil {
-		http.Error(w, "File write failure", http.StatusInternalServerError)
+	timestamp := time.Now().UTC().Format("2006-01-02T15-04-05")
+	ext := filepath.Ext(handler.Filename)
+	filename := fmt.Sprintf("%s_converted%s", timestamp, ext)
+
+	if err := os.WriteFile(filename, []byte(converted), 0644); err != nil {
+		http.Error(w, "File write error", http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write(data)
+	w.Write([]byte(converted))
 }
